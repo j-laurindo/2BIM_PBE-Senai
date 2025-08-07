6d719ddfee58388ac9dd443ef20c70d0d105709d
j-laurindo <juliarobertslaurindodasilva@gmail.com>
2025-08-07 16:44:55 -0300

    criação da tabela Autor - models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    nome  = models.CharField(max_length=255)    
+    sobrenome = models.CharField(max_length=255) 
+    data_nascimento = models.DateField(null=True, blank=True)  
+    nacionalidade = models.CharField(max_length=100, null=True, blank=True)
+    biografia = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
+
+           
+             
+
+
