402b4a7fa03fb2bb0dddad2233dc0349dde490da
j-laurindo <juliarobertslaurindodasilva@gmail.com>
2025-08-14 16:54:24 -0300

    api_view

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import * 
 
 urlpatterns = [
-    path('autores', AutoresView.as_view())
-    
+    path('autores', AutoresView.as_view()),
+    path('authors', list_autores)
 ]
