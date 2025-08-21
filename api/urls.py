de939a119273d79c56ca1a452dcc9c70ceb32877
j-laurindo <juliarobertslaurindodasilva@gmail.com>
2025-08-21 16:50:54 -0300

    Requisições HTTP - UPDATE e DELETE

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from .views import * 
 
 urlpatterns = [
+    #GET / POST
     path('autores', AutoresView.as_view()),
     path('editora', EditoraView.as_view()),
-    path('livros', LivrosView.as_view())
+    path('livros', LivrosView.as_view()),
+
+    #UPDATE / DELETE
+    path('autor/<int:pk>', AutoresDetailView.as_view()),
+    path('editora/<int:pk>', EditorasDetailView.as_view()),
+    path('livro/<int:pk>', LivrosDetailView.as_view()),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,23 +1,47 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Autor, Editora, Livro
 from .serializers import AutorSerializers, EditoraSerializers, LivrosSerializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+##################### AUTORES ###############################
 class AutoresView(ListCreateAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializers
 
+class AutoresDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializers
+#############################################################
+
+
+################### EDITORA #################################
 class EditoraView(ListCreateAPIView):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializers
 
+class EditorasDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializers
+#############################################################
+
+
+################## LIVROS ###################################
 class LivrosView(ListCreateAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivrosSerializers
 
+class LivrosDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivrosSerializers
+
+#############################################################
+
+
+
+######## EXEMPLO API_VIEW #########################################
 @api_view(['GET', 'POST'])
 def list_autores(request):
     if request.method == 'GET':
